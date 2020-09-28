285d858bbac6130f2755b3120b9329f8f5b92298
Kynan Stevenson <kynants@gmail.com>
2020-09-28 04:49:19 -0400

    Initial commit

---
+++ site.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Exercise 2</title>
+</head>
+<body>
+  <?php
+    echo "<p>Tomorrow I'll learn PHP global variables.</p>\n";
+    echo "<p>This is a bad command : del c:\\*.*</p>";
+  ?>
+</body>
+</html>
